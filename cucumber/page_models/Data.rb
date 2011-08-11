ec677c0b863c75383e863f08119d5b6f43c15a10
w12495 <w12495>
2011-08-11 00:22:06 +0000

    Updated cucumber tests to test wpm iphone scrapable link and to move xpaths out of the model and into the data.

--- cucumber/page_models/Data.rb
+++ cucumber/page_models/Data.rb
@@ -3,30 +3,49 @@ module Pages
     def self.init_data
 
       data = {}
-      data["a phone number"] = PhoneNumber.new("clicktocall-ph0", "(03) 9001 0001", "+61390010001", 
+
+      data["a phone number"] = PhoneNumber.new("clicktocall-ph0", "(03) 9001 0001", "+61390010001",  false,
                                                 "au.com.sensis.mobile.web.component.clicktocall.showcase.presentation.action.CallAction {} \
 - AJAX reporting call for phone number '+61390010001' - in a real app, reporting would occur here ...")
-      data["another phone number"] = PhoneNumber.new("clicktocall-ph1", "(03) 9001 0002", "+61390010002",
+
+      data["another phone number"] = PhoneNumber.new("clicktocall-ph2", "(03) 9001 0002", "+61390010002", false,
                                                 "au.com.sensis.mobile.web.component.clicktocall.showcase.presentation.action.CallAction {} \
 - AJAX reporting call for phone number '+61390010002' - in a real app, reporting would occur here ...")
-      data["WPM iphone scrapable phone number"] = PhoneNumber.new("phoneNumber", "(02) 7001 0002", "+61270010002",
+
+      data["WPM iphone scrapable phone number"] = PhoneNumber.new("phoneNumberWpm", "(02) 7001 0002", "+61270010002", true,
                                                 "au.com.sensis.mobile.web.component.clicktocall.showcase.presentation.action.CallAction {} \
 - AJAX reporting call for phone number '+61270010002' - in a real app, reporting would occur here ...")
+
       data
     end
 
     class PhoneNumber
-      attr_accessor :container_id, :link_text, :call_formatted_number, :log_string
+      attr_accessor :container_id, :link_text, :call_formatted_number, :iphone_scrappable_wpm, :log_string
 
-      def initialize(container_id, link_text, call_formatted_number, log_string)
+      def initialize(container_id, link_text, call_formatted_number, iphone_scrappable_wpm, log_string)
         @container_id = container_id
         @link_text = link_text
         @call_formatted_number = call_formatted_number
+        @iphone_scrappable_wpm = iphone_scrappable_wpm
         @log_string = log_string
       end
 
       def href(device)
         "#{device.call_link_protocol_string}#{@call_formatted_number}"
+      end
+
+      def phone_number_link_xpath
+        if (iphone_scrappable_wpm)
+          # Don't assert the value of the href. The actual value doesn't matter as long as clicking it has the desired effet.
+          "//div[@id='#{@container_id}' and @class='callLink']/a[@class='clickToCallLink']/span[@id='phoneNumber' and . = '#{@link_text}']"
+        else
+          # Don't assert the value of the href. The actual value doesn't matter as long as clicking it has the desired effet.
+          "//div[@id='#{@container_id}' and @class='callLink']/a[@class='clickToCallLink' and . = '#{@link_text}']"
+        end
+      end
+
+      def phone_number_no_link_xpath
+        "//div[@id='#{@container_id}' and @class='callLink' and . = '#{@link_text}']"
       end
     end
 
--- cucumber/page_models/HomePageModel.rb
+++ cucumber/page_models/HomePageModel.rb
@@ -20,14 +20,13 @@ module Pages
     def has_phone_number_links?
       i = 0
       @data.each_value do |data_item|
-        # Don't assert the value of the href. The actual value doesn't matter as long as clicking it has the desired effet.
-        @capybara_page.should have_xpath("//div[@id='#{data_item.container_id}' and @class='callLink' and a = '#{data_item.link_text}']/a[@class='clickToCallLink']")
+        @capybara_page.should have_xpath(data_item.phone_number_link_xpath)
       end
     end
 
     def has_no_phone_number_links?
       @data.each_value do |data_item|
-        @capybara_page.should have_xpath('//div[@id="clicktocall-ph0" and @class="callLink" and . = "(03) 9001 0001"]')
+        @capybara_page.should have_xpath(data_item.phone_number_no_link_xpath)
       end
     end
 
